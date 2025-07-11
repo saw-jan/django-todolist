6c81278928e5069bd8e536b0d12df8479850b12f
Saw-jan <saw.jan.grg3e@gmail.com>
2025-07-11 15:49:47 +0545

    use postgres

--- todolist/settings.py
+++ todolist/settings.py
@@ -65,8 +65,12 @@ WSGI_APPLICATION = "todolist.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": os.getenv("DJANGO_DB_NAME", ""),
+        "USER": os.getenv("DJANGO_DB_USER", ""),
+        "PASSWORD": os.getenv("DJANGO_DB_PASSWORD", ""),
+        "HOST": os.getenv("DJANGO_DB_HOST", ""),
+        "PORT": os.getenv("DJANGO_DB_PORT", ""),
     }
 }
 
